8bf11e87ff765b7e181a3b68548b3c01154671fd
c325006 <c325006>
2023-04-21 17:49:04 +0200

    wip unit tests

---
+++ src/test/java/com/giuliolongfils/agitation/config/YamlParserTest.java
@@ -0,0 +1,20 @@
+package com.giuliolongfils.agitation.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("YamlParser")
+class YamlParserTest {
+
+    @InjectMocks
+    private YamlParser yamlParser;
+
+    @Test
+    @DisplayName("")
+    public void read() {
+
+    }
+}
