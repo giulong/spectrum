7ee7e3cde2380a47ca6fb5dd3527cbe11c357ac7
Giulio Longfils <giuliolongfils@gmail.com>
2023-04-22 13:13:00 +0200

    unit tests

---
+++ src/test/java/com/giuliolongfils/agitation/TestYaml.java
@@ -0,0 +1,14 @@
+package com.giuliolongfils.agitation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestYaml {
+    private String key;
+}
