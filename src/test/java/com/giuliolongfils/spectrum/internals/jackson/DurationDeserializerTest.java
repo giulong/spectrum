0e659c68ab510cb8e67021028ebcdef43b4d22ea
c325006 <c325006>
2023-05-17 17:58:28 +0200

    unit tests

---
+++ src/test/java/com/giuliolongfils/spectrum/internals/jackson/DurationDeserializerTest.java
@@ -0,0 +1,39 @@
+package com.giuliolongfils.spectrum.internals.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("DurationDeserializer")
+class DurationDeserializerTest {
+
+    @Mock
+    private JsonParser jsonParser;
+
+    @Mock
+    private DeserializationContext deserializationContext;
+
+    @InjectMocks
+    private DurationDeserializer durationDeserializer;
+
+    @Test
+    @DisplayName("deserialize should return the duration in seconds from the provided string")
+    public void deserialize() throws IOException {
+        int value = 123;
+        when(jsonParser.getValueAsInt()).thenReturn(value);
+
+        assertEquals(Duration.ofSeconds(value), durationDeserializer.deserialize(jsonParser, deserializationContext));
+    }
+}
---
+++ src/test/java/com/giuliolongfils/spectrum/internals/jackson/UtilLogLevelDeserializerTest.java
@@ -0,0 +1,39 @@
+package com.giuliolongfils.spectrum.internals.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static java.util.logging.Level.INFO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("UtilLogLevelDeserializer")
+class UtilLogLevelDeserializerTest {
+
+    @Mock
+    private JsonParser jsonParser;
+
+    @Mock
+    private DeserializationContext deserializationContext;
+
+    @InjectMocks
+    private UtilLogLevelDeserializer UtilLogLevelDeserializer;
+
+    @Test
+    @DisplayName("deserialize should return the log level from the provided string")
+    public void deserialize() throws IOException {
+        String value = "INFO";
+        when(jsonParser.getValueAsString()).thenReturn(value);
+
+        assertEquals(INFO, UtilLogLevelDeserializer.deserialize(jsonParser, deserializationContext));
+    }
+}
