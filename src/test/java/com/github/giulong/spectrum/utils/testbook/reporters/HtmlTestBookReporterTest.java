1f8d58f5ce9cb04491aaabc79235125025aedf35
Giulio Longfils <giuliolongfils@gmail.com>
2023-05-30 19:16:38 +0200

    fixing unit tests

--- src/test/java/com/github/giulong/spectrum/utils/testbook/reporters/HtmlTestBookReporterTest.java
+++ src/test/java/com/github/giulong/spectrum/utils/testbook/reporters/HtmlTestBookReporterTest.java
@@ -51,7 +51,8 @@ class HtmlTestBookReporterTest {
         filesMockedStatic.verify(() -> Files.createDirectories(Path.of(output).getParent()));
         filesMockedStatic.verify(() -> {
             Files.write(pathArgumentCaptor.capture(), eq(interpolatedTemplate.getBytes()));
-            assertThat(pathArgumentCaptor.getValue().toString(), matchesPattern(output.replace("{timestamp}.html", "[0-9]{2}-[0-9]{2}-[0-9]{4}_[0-9]{2}-[0-9]{2}-[0-9]{2}.html")));
+            assertThat(pathArgumentCaptor.getValue().toString().replace("\\", "/"),
+                    matchesPattern(output.replace("{timestamp}.html", "[0-9]{2}-[0-9]{2}-[0-9]{4}_[0-9]{2}-[0-9]{2}-[0-9]{2}.html")));
         });
     }
 }
