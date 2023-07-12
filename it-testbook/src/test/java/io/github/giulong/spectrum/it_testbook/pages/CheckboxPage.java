92130ea1d1db31175e8708ce33c4f58f21327860
Giulio Longfils <giuliolongfils@gmail.com>
2023-07-12 17:34:14 +0200

    It Testbook verifier test

---
+++ it-testbook/src/test/java/io/github/giulong/spectrum/it_testbook/pages/CheckboxPage.java
@@ -0,0 +1,21 @@
+package io.github.giulong.spectrum.it_testbook.pages;
+
+import io.github.giulong.spectrum.SpectrumPage;
+import io.github.giulong.spectrum.interfaces.Endpoint;
+import lombok.Getter;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+import java.util.List;
+
+@Getter
+@Endpoint("checkboxes")
+public class CheckboxPage extends SpectrumPage<CheckboxPage, Void> {
+
+    @FindBys({
+            @FindBy(id = "checkboxes"),
+            @FindBy(tagName = "input")
+    })
+    private List<WebElement> checkboxes;
+}
