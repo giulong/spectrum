fb4373cb9aabcdd5410e335096603aa8eb498ef4
Giulio Longfils <giuliolongfils@gmail.com>
2023-11-17 21:37:35 +0100

    custom log colors

---
+++ spectrum/src/main/java/io/github/giulong/spectrum/internals/ColoredConverter.java
@@ -0,0 +1,21 @@
+package io.github.giulong.spectrum.internals;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+import static ch.qos.logback.classic.Level.*;
+import static ch.qos.logback.core.pattern.color.ANSIConstants.*;
+
+public class ColoredConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+
+    @Override
+    protected String getForegroundColorCode(final ILoggingEvent event) {
+        return switch (event.getLevel().toInt()) {
+            case ERROR_INT -> BOLD + RED_FG;
+            case WARN_INT -> YELLOW_FG;
+            case INFO_INT -> BLUE_FG;
+            case TRACE_INT -> MAGENTA_FG;
+            default -> DEFAULT_FG;
+        };
+    }
+}
