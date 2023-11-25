f23e38c00eb7f299e9d61290162d6e06c2ec7dc9
Giulio Longfils <giuliolongfils@gmail.com>
2023-11-25 19:38:56 +0100

    try-catching in copyCode.js to avoid breaking all buttons

--- docs/assets/scripts/copyCode.js
+++ docs/assets/scripts/copyCode.js
@@ -2,13 +2,17 @@ const copyHeaders = document.querySelectorAll('.copy-header');
 const copyButtons = document.querySelectorAll('.copy-button');
 
 copyButtons.forEach((copyButton, index) => {
-  const code = copyHeaders[index].parentElement.nextElementSibling.innerText;
+    try {
+        const code = copyHeaders[index].parentElement.nextElementSibling.innerText;
 
-  copyButton.addEventListener('click', () => {
-    window.navigator.clipboard.writeText(code);
-    const copyText = copyButton.querySelectorAll('.copy-text')[0];
-    copyText.style.display = 'inline';
+        copyButton.addEventListener('click', () => {
+        window.navigator.clipboard.writeText(code);
+        const copyText = copyButton.querySelectorAll('.copy-text')[0];
+        copyText.style.display = 'inline';
 
-    setTimeout(() => copyText.style.display = 'none', 2000);
-  });
+        setTimeout(() => copyText.style.display = 'none', 2000);
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
