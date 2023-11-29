bd9703b28eaa76d490484155ce37046ce99ae08b
Giulio Longfils <giuliolongfils@gmail.com>
2023-11-29 15:54:00 +0100

    Internal JsonSchema for internal configuration

--- spectrum/src/main/java/io/github/giulong/spectrum/internals/jackson/json_schema/JsonSchemaGeneratorModule.java
+++ spectrum/src/main/java/io/github/giulong/spectrum/internals/jackson/json_schema/JsonSchemaGeneratorModule.java
@@ -3,76 +3,41 @@ package io.github.giulong.spectrum.internals.jackson.json_schema;
 import com.fasterxml.classmate.ResolvedType;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonView;
-import com.github.victools.jsonschema.generator.Module;
-import com.github.victools.jsonschema.generator.*;
-import com.github.victools.jsonschema.module.jackson.JacksonModule;
+import com.github.victools.jsonschema.generator.FieldScope;
+import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder;
+import com.github.victools.jsonschema.generator.TypeContext;
 import com.github.victools.jsonschema.module.jackson.JsonSubTypesResolver;
-import io.github.giulong.spectrum.interfaces.JsonSchemaTypes;
 import io.github.giulong.spectrum.internals.jackson.views.Views;
-import io.github.giulong.spectrum.pojos.Configuration;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static com.github.victools.jsonschema.generator.Option.FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT;
-import static com.github.victools.jsonschema.generator.Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES;
-import static com.github.victools.jsonschema.module.jackson.JacksonOption.FLATTENED_ENUMS_FROM_JSONVALUE;
-import static com.github.victools.jsonschema.module.jackson.JacksonOption.SKIP_SUBTYPE_LOOKUP;
 import static java.util.stream.Collectors.toList;
 
 @Slf4j
-public class JsonSchemaGeneratorModule implements Module {
+public class JsonSchemaGeneratorModule extends JsonSchemaInternalGeneratorModule {
 
-    @SneakyThrows
     @Override
     public void applyToConfigBuilder(final SchemaGeneratorConfigBuilder schemaGeneratorConfigBuilder) {
-        schemaGeneratorConfigBuilder
-                .with(new JacksonModule(SKIP_SUBTYPE_LOOKUP, FLATTENED_ENUMS_FROM_JSONVALUE))
-                .with(FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT)
-                .with(MAP_VALUES_AS_ADDITIONAL_PROPERTIES);
+        super.applyToConfigBuilder(schemaGeneratorConfigBuilder);
 
         schemaGeneratorConfigBuilder
                 .forFields()
-                .withIgnoreCheck(this::jsonViewInternalPredicate)
-                .withTargetTypeOverridesResolver(this::multipleTypesResolver);
+                .withIgnoreCheck(this::jsonViewInternalPredicate);
 
         schemaGeneratorConfigBuilder
                 .forTypesInGeneral()
                 .withSubtypeResolver(new PublicSubTypeResolver());
 
-        final String jsonSchema = new SchemaGenerator(schemaGeneratorConfigBuilder.build()).generateSchema(Configuration.class).toString();
-        final URI targetFolderUri = JsonSchemaGeneratorModule.class.getProtectionDomain().getCodeSource().getLocation().toURI();
-        final String targetFolder = Path.of(targetFolderUri).getParent().toString();
-        final Path jsonSchemaPath = Path.of(targetFolder, "json-schemas", "Configuration.json");
-
-        //noinspection ResultOfMethodCallIgnored
-        jsonSchemaPath.getParent().toFile().mkdirs();
-        Files.writeString(jsonSchemaPath, jsonSchema);
+        writeSchema(schemaGeneratorConfigBuilder, "Configuration.json");
     }
 
     private static boolean isInternal(final AnnotatedElement annotatedElement) {
         return annotatedElement.isAnnotationPresent(JsonView.class) && Arrays.asList(annotatedElement.getAnnotation(JsonView.class).value()).contains(Views.Internal.class);
-    }
-
-    private List<ResolvedType> multipleTypesResolver(final FieldScope field) {
-        final JsonSchemaTypes jsonSchemaTypes = field.getAnnotationConsideringFieldAndGetterIfSupported(JsonSchemaTypes.class);
-
-        if (jsonSchemaTypes == null) {
-            return null;
-        }
-
-        return Arrays
-                .stream(jsonSchemaTypes.value())
-                .map(c -> field.getContext().resolve(c))
-                .toList();
     }
 
     private boolean jsonViewInternalPredicate(final FieldScope fieldScope) {
---
+++ spectrum/src/main/java/io/github/giulong/spectrum/internals/jackson/json_schema/JsonSchemaInternalGeneratorModule.java
@@ -0,0 +1,68 @@
+package io.github.giulong.spectrum.internals.jackson.json_schema;
+
+import com.fasterxml.classmate.ResolvedType;
+import com.github.victools.jsonschema.generator.*;
+import com.github.victools.jsonschema.generator.Module;
+import com.github.victools.jsonschema.module.jackson.JacksonModule;
+import com.github.victools.jsonschema.module.jackson.JsonSubTypesResolver;
+import io.github.giulong.spectrum.interfaces.JsonSchemaTypes;
+import io.github.giulong.spectrum.pojos.Configuration;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.victools.jsonschema.generator.Option.FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT;
+import static com.github.victools.jsonschema.generator.Option.MAP_VALUES_AS_ADDITIONAL_PROPERTIES;
+import static com.github.victools.jsonschema.module.jackson.JacksonOption.FLATTENED_ENUMS_FROM_JSONVALUE;
+import static com.github.victools.jsonschema.module.jackson.JacksonOption.SKIP_SUBTYPE_LOOKUP;
+
+@Slf4j
+public class JsonSchemaInternalGeneratorModule implements Module {
+
+    @Override
+    public void applyToConfigBuilder(final SchemaGeneratorConfigBuilder schemaGeneratorConfigBuilder) {
+        schemaGeneratorConfigBuilder
+                .with(new JacksonModule(SKIP_SUBTYPE_LOOKUP, FLATTENED_ENUMS_FROM_JSONVALUE))
+                .with(FORBIDDEN_ADDITIONAL_PROPERTIES_BY_DEFAULT, MAP_VALUES_AS_ADDITIONAL_PROPERTIES);
+
+        schemaGeneratorConfigBuilder
+                .forFields()
+                .withTargetTypeOverridesResolver(this::multipleTypesResolver);
+
+        schemaGeneratorConfigBuilder
+                .forTypesInGeneral()
+                .withSubtypeResolver(new JsonSubTypesResolver());
+
+        writeSchema(schemaGeneratorConfigBuilder, "ConfigurationInternal.json");
+    }
+
+    @SneakyThrows
+    protected void writeSchema(final SchemaGeneratorConfigBuilder schemaGeneratorConfigBuilder, final String name) {
+        final String jsonSchema = new SchemaGenerator(schemaGeneratorConfigBuilder.build()).generateSchema(Configuration.class).toString();
+        final URI targetFolderUri = JsonSchemaInternalGeneratorModule.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+        final String targetFolder = Path.of(targetFolderUri).getParent().toString();
+        final Path jsonSchemaPath = Path.of(targetFolder, "json-schemas", name);
+
+        //noinspection ResultOfMethodCallIgnored
+        jsonSchemaPath.getParent().toFile().mkdirs();
+        Files.writeString(jsonSchemaPath, jsonSchema);
+    }
+
+    private List<ResolvedType> multipleTypesResolver(final FieldScope field) {
+        final JsonSchemaTypes jsonSchemaTypes = field.getAnnotationConsideringFieldAndGetterIfSupported(JsonSchemaTypes.class);
+
+        if (jsonSchemaTypes == null) {
+            return null;
+        }
+
+        return Arrays
+                .stream(jsonSchemaTypes.value())
+                .map(c -> field.getContext().resolve(c))
+                .toList();
+    }
+}
