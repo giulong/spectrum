506c67905029f0307cbbaf508b88806d139e48cc
Giulio Longfils <giuliolongfils@gmail.com>
2023-12-02 22:37:03 +0100

    docs scroll fix

--- docs/assets/scripts/topButton.js
+++ docs/assets/scripts/topButton.js
@@ -1,16 +1,3 @@
-const topButton = document.getElementById("topButton");
-const topButtonOffset = 350;
-
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > topButtonOffset || document.documentElement.scrollTop > topButtonOffset) {
-        topButton.style.display = 'block';
-    } else {
-        topButton.style.display = 'none';
-    }
-}
-
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
