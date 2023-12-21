2f7900a291bbb68f80690d8b6db3d8a8e5e26250
Giulio Longfils <giuliolongfils@gmail.com>
2023-12-21 23:16:40 +0100

    better left menu styling in docs + toggle button

--- docs/assets/scripts/leftMenu.js
+++ docs/assets/scripts/leftMenu.js
@@ -1,19 +1,31 @@
+const leftMenu = document.getElementById('left-menu');
 const tocList = document.getElementById('toc-list');
 const tocElements = Array.from(document.querySelectorAll('h1, h2, h3, h4'));
+const toggleTocButton = document.getElementById('toggle-toc-button');
 
-document.onload = leftMenu();
+document.onload = buildLeftMenu();
 
-function leftMenu() {
+function buildLeftMenu() {
     tocElements
         .map(tocElement => {
             const li = document.createElement('li');
             const text = tocElement.innerText.toLowerCase();
 
             li.setAttribute('onclick', 'navigateTo("' + text.replaceAll(' ', '-') + '")');
-            li.classList.add('search-result', tocElement.nodeName.toLowerCase());
+            li.classList.add('toc-element', tocElement.nodeName.toLowerCase());
             li.innerText = text;
 
             return li;
         })
         .forEach(li => tocList.appendChild(li));
+}
+
+function toggleToc() {
+    if (getComputedStyle(leftMenu).opacity == 1) {
+        leftMenu.style.opacity = 0;
+        toggleTocButton.style.left = '15px';
+    } else {
+        leftMenu.style.opacity = 1;
+        toggleTocButton.style.left = '315px';
+    }
 }
