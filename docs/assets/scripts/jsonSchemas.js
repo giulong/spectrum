1b0e5acaa4ceb7c319c9ececdf4272f5aa58d3de
Giulio Longfils <giuliolongfils@gmail.com>
2024-03-10 16:23:51 +0100

    expandable json schemas old versions

--- docs/assets/scripts/jsonSchemas.js
+++ docs/assets/scripts/jsonSchemas.js
@@ -4,6 +4,7 @@
     const jsonSchemasBody = document.getElementById('json-schemas-body');
     const response = await fetch('https://api.github.com/repos/giulong/spectrum/contents/docs/json-schemas');
     const json = await response.json();
+    const latestJson = json.pop();
     const rows = json
         .map(jsonSchema => {
             const url = "{{ site.docs_url }}/{{ site.json_schemas_endpoint }}" + jsonSchema.name + "/Configuration-schema.json";
@@ -14,6 +15,10 @@
         })
         .reverse()
         .join('</tr><tr>');
+
+    const latestUrl = "{{ site.docs_url }}/{{ site.json_schemas_endpoint }}" + latestJson.name + "/Configuration-schema.json";
+    document.getElementById('latest-json-schema-version').innerHTML = '<a href="' + latestUrl + '" target="_blank">' + latestJson.name + '</a>';
+    document.getElementById('latest-json-schema-path').innerHTML = latestUrl;
 
     jsonSchemasBody.innerHTML = '<tr>' + rows + '</tr>';
 })()
