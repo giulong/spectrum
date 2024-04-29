f317e5e67459a89f28281e1b765367161c83ef1c
AndreaColomboIT <andreacolombo.dev@gmail.com>
2024-04-29 22:32:46 +0200

    Added part of the unit tests for Js Class

--- spectrum/src/test/java/io/github/giulong/spectrum/utils/JsTest.java
+++ spectrum/src/test/java/io/github/giulong/spectrum/utils/JsTest.java
@@ -32,4 +32,29 @@ class JsTest {
 
         verify(webDriver).executeScript("arguments[0].click();", webElement);
     }
+
+    @Test
+    @DisplayName("sendkeys should insert the input string as value of the provided webElement and return the Js instance")
+    public void sendKeys() {
+        final String keysToSend = "input Test";
+        assertEquals(js, js.sendKeys(webElement, keysToSend));
+
+        verify(webDriver).executeScript(String.format("arguments[0].value='%s';", keysToSend), webElement);
+    }
+
+    @Test
+    @DisplayName("submit should submit with javascript the provided webElement and return the Js instance")
+    public void submit() {
+        assertEquals(js, js.submit(webElement));
+
+        verify(webDriver).executeScript("arguments[0].submit();", webElement);
+    }
+
+    @Test
+    @DisplayName("clear should delete the values with javascript from the provided webElement and return the Js instance")
+    public void clear() {
+        assertEquals(js, js.clear(webElement));
+
+        verify(webDriver).executeScript("arguments[0].value='';", webElement);
+    }
 }
