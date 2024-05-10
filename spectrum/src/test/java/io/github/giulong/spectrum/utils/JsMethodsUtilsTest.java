e611aa238ecaa7d11bdedc3047a12a3d8fdae970
AndreaColomboIT <andreacolombo.dev@gmail.com>
2024-05-10 23:44:24 +0200

    added Unit tests for JsMethodUtils

--- spectrum/src/test/java/io/github/giulong/spectrum/utils/JsMethodsUtilsTest.java
+++ spectrum/src/test/java/io/github/giulong/spectrum/utils/JsMethodsUtilsTest.java
@@ -31,12 +31,29 @@ public class JsMethodsUtilsTest {
     }
 
     @Test
-    @DisplayName("getScript should return the correct js script by the provided locator")
+    @DisplayName("getFindElementScript should return the correct js script by the provided locator for findElement method")
     void testGetFindElementScript() {
+        assertEquals("return %s.getElementById('%s');", jsMethodsUtils.getFindElementScript(LocatorType.Id));
+        assertEquals("return %s.querySelector('%s');", jsMethodsUtils.getFindElementScript(LocatorType.cssSelector));
+        assertEquals("return %s.evaluate('%s', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;", jsMethodsUtils.getFindElementScript(LocatorType.xpath));
+        assertEquals("return %s.evaluate('//a[contains(text(), \"%s\")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;", jsMethodsUtils.getFindElementScript(LocatorType.partialLinkText));
         assertEquals("return %s.getElementsByTagName('%s')[0];", jsMethodsUtils.getFindElementScript(LocatorType.tagName));
         assertEquals("return %s.getElementsByClassName('%s')[0];", jsMethodsUtils.getFindElementScript(LocatorType.className));
         assertEquals("return %s.evaluate('//a[text()=\"%s\"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;", jsMethodsUtils.getFindElementScript(LocatorType.linkText));
         assertNotEquals("return %s.evaluate('//a[text()=\"%s\"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;", jsMethodsUtils.getFindElementScript(LocatorType.name));
+    }
+
+    @Test
+    @DisplayName("getFindElementsScript should return the correct js script by the provided locator for findElements method")
+    void testGetFindElementsScript() {
+        assertEquals("return %s.querySelectorAll('#%s');", jsMethodsUtils.getFindElementsScript(LocatorType.Id));
+        assertEquals("return %s.getElementsByClassName('%s');", jsMethodsUtils.getFindElementsScript(LocatorType.className));
+        assertEquals("return %s.getElementsByTagName('%s');", jsMethodsUtils.getFindElementsScript(LocatorType.tagName));
+        assertNotEquals("jreturn %s.querySelectorAll('[name=\\\"%s\\\"]');", jsMethodsUtils.getFindElementsScript(LocatorType.name));
+        assertEquals("return %s.querySelectorAll('%s');", jsMethodsUtils.getFindElementsScript(LocatorType.cssSelector));
+        assertEquals("var webElements = %s.evaluate('%s', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);return Array.from({length: webElements.snapshotLength}, (_, i) => webElements.snapshotItem(i));", jsMethodsUtils.getFindElementsScript(LocatorType.xpath));
+        assertEquals("return Array.from(%s.querySelectorAll('a')).filter(link => link.textContent === '%s');", jsMethodsUtils.getFindElementsScript(LocatorType.linkText));
+        assertEquals("return Array.from(%s.querySelectorAll('a')).filter(link => link.textContent.includes('%s'));", jsMethodsUtils.getFindElementsScript(LocatorType.partialLinkText));
     }
 
     @Test
