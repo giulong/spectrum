1d4fd0e17a0a9f9d217eba8cb10ad7dd121a77c2
Giulio Longfils <giuliolongfils@gmail.com>
2024-12-14 21:45:19 +0100

    docs: displaying default generic json schema version url on error, like when gh rest api limit is reached

--- docs/assets/scripts/jsonSchemas.js
+++ docs/assets/scripts/jsonSchemas.js
@@ -17,59 +17,64 @@ function compareSemanticVersions(a, b) {
 }
 
 const copyCodeHtml = `
-<td>
-    <span class="schema-copy-header">
-        <button class="schema-copy-button">
-            <span class="schema-copy-text" style="display: none;">Copied!</span>
-            <img class="copy-icon" src="assets/images/copy.png" alt="copy"/>
-        </button>
-    </span>
-</td>`;
+<span class="schema-copy-header">
+    <button class="schema-copy-button">
+        <span class="schema-copy-text" style="display: none;">Copied!</span>
+        <img class="copy-icon" src="assets/images/copy.png" alt="copy"/>
+    </button>
+</span>`;
 
 (async () => {
-    const jsonSchemasBody = document.getElementById('json-schemas-body');
-    const response = await fetch('https://api.github.com/repos/giulong/spectrum/contents/docs/json-schemas');
-    const json = await response.json();
-    const sortedJson = json.sort(compareSemanticVersions);
-    const latestJson = sortedJson.pop();
-    const rows = sortedJson
-        .map(jsonSchema => {
-            const url = "{{ site.docs_url }}/{{ site.json_schemas_endpoint }}" + jsonSchema.name + "/Configuration-schema.json";
-            const versionColumn = '<td><a href="' + url + '" target="_blank">' + jsonSchema.name + '</a></td>';
-            const fullPathColumn = '<td>' + url + '</td>';
+    fetch('https://api.github.com/repos/giulong/spectrum/contents/docs/json-schemas')
+        .then(response => response.json())
+        .then(json => json.sort(compareSemanticVersions))
+        .then(json => {
+            const latestJson = json.pop();
+            const rows = json
+                .map(jsonSchema => {
+                    const url = "{{ site.docs_url }}/{{ site.json_schemas_endpoint }}" + jsonSchema.name + "/Configuration-schema.json";
+                    const versionColumn = '<td><a href="' + url + '" target="_blank">' + jsonSchema.name + '</a></td>';
+                    const fullPathColumn = '<td>' + url + '</td>';
+                    const copyCodeColumn = '<td>' + copyCodeHtml + '</td>';
 
-            return versionColumn + fullPathColumn + copyCodeHtml;
+                    return versionColumn + fullPathColumn + copyCodeColumn;
+                })
+                .reverse()
+                .join('</tr><tr>');
+
+            const latestUrl = "{{ site.docs_url }}/{{ site.json_schemas_endpoint }}" + latestJson.name + "/Configuration-schema.json";
+            document.getElementById('latest-json-schema-version').innerHTML = '<a href="' + latestUrl + '" target="_blank">' + latestJson.name + '</a>';
+            document.getElementById('latest-json-schema-path').innerText = latestUrl;
+            document.getElementById('latest-json-schema-copy-header').innerHTML = copyCodeHtml;
+            document.getElementById('json-schemas-body').innerHTML = '<tr>' + rows + '</tr>';
         })
-        .reverse()
-        .join('</tr><tr>');
+        .catch(error => {
+            document.getElementById('latest-json-schema-version').innerText = 'N/A';
+            document.getElementById('latest-json-schema-path').innerText = 'https://giulong.github.io/spectrum/json-schemas/<VERSION>/Configuration-schema.json';
+            document.getElementById('latest-json-schema-copy-header').innerHTML = copyCodeHtml;
+        })
+        .finally(() => {
+            const jsonSchemaHeaders = document.querySelectorAll('.schema-copy-header');
+            const jsonSchemaButtons = document.querySelectorAll('.schema-copy-button');
+            jsonSchemaButtons.forEach((copyButton, index) => {
+                try {
+                    const url = jsonSchemaHeaders[index].parentElement.previousElementSibling.textContent;
+                    copyButton.addEventListener('click', () => {
+                        window.navigator.clipboard.writeText(url);
+                        const copyText = copyButton.querySelector('.schema-copy-text');
+                        const copyIcon = copyButton.querySelector('.copy-icon');
 
-    const latestUrl = "{{ site.docs_url }}/{{ site.json_schemas_endpoint }}" + latestJson.name + "/Configuration-schema.json";
-    document.getElementById('latest-json-schema-version').innerHTML = '<a href="' + latestUrl + '" target="_blank">' + latestJson.name + '</a>';
-    document.getElementById('latest-json-schema-path').innerHTML = latestUrl;
+                        copyText.style.display = 'inline';
+                        copyIcon.src = 'assets/images/check.png';
 
-    jsonSchemasBody.innerHTML = '<tr>' + rows + '</tr>';
-
-    const jsonSchemaHeaders = document.querySelectorAll('.schema-copy-header');
-    const jsonSchemaButtons = document.querySelectorAll('.schema-copy-button');
-    jsonSchemaButtons.forEach((copyButton, index) => {
-        try {
-            const url = jsonSchemaHeaders[index].parentElement.previousElementSibling.innerText;
-
-            copyButton.addEventListener('click', () => {
-                window.navigator.clipboard.writeText(url);
-                const copyText = copyButton.querySelector('.schema-copy-text');
-                const copyIcon = copyButton.querySelector('.copy-icon');
-
-                copyText.style.display = 'inline';
-                copyIcon.src = 'assets/images/check.png';
-
-                setTimeout(() => {
-                    copyText.style.display = 'none';
-                    copyIcon.src = 'assets/images/copy.png';
-                }, 2000);
+                        setTimeout(() => {
+                            copyText.style.display = 'none';
+                            copyIcon.src = 'assets/images/copy.png';
+                        }, 2000);
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
             });
-        } catch (error) {
-            console.error(error);
-        }
-    });
+        });
 })()
