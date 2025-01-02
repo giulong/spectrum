f49f376c4d99fdd377fe04bb091c4d78a8143cac
Giulio Longfils <giuliolongfils@gmail.com>
2025-01-02 16:49:35 +0100

    fix: ignoring exceptions when checking secured web elements that are not in the currently displayed page

--- it/src/test/java/io/github/giulong/spectrum/it/tests/LoginFormIT.java
+++ it/src/test/java/io/github/giulong/spectrum/it/tests/LoginFormIT.java
@@ -1,12 +1,15 @@
 package io.github.giulong.spectrum.it.tests;
 
+import io.github.giulong.spectrum.it.pages.InputsPage;
 import io.github.giulong.spectrum.it.pages.LoginPage;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebElement;
 
 import java.util.Objects;
 import java.util.stream.Stream;
@@ -15,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.Keys.ARROW_UP;
 import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
 
 @Slf4j
@@ -23,6 +27,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
 public class LoginFormIT extends BaseIT {
 
     private LoginPage loginPage;
+    private InputsPage inputsPage;
 
     @BeforeEach
     public void beforeEach() {
@@ -60,5 +65,19 @@ public class LoginFormIT extends BaseIT {
                 arguments("tom", true, "/secure"),
                 arguments("giulio", false, "/login")
         );
+    }
+
+    @Test
+    @DisplayName("even if we have pages with @Secure web elements (password in LoginPage), tests that don't use them must not fail")
+    void inputs() {
+        final String number = "2";
+        final WebElement input = inputsPage.getInput();
+
+        inputsPage.open();
+
+        input.sendKeys(number);
+        input.sendKeys(ARROW_UP);
+
+        assertEquals("3", input.getDomProperty("value"));
     }
 }
