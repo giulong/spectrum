12ba1e1a1ca1a762a23d0624694eed238703003f
Giulio Longfils <giuliolongfils@gmail.com>
2025-11-23 16:00:03 +0100

    test: better checking that vars are parsed before the rest, and injected in other configuration keys

--- it-grid/src/test/java/io/github/giulong/spectrum/it_grid/tests/FilesIT.java
+++ it-grid/src/test/java/io/github/giulong/spectrum/it_grid/tests/FilesIT.java
@@ -2,7 +2,9 @@ package io.github.giulong.spectrum.it_grid.tests;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 import java.io.IOException;
@@ -10,8 +12,11 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -50,6 +55,10 @@ class FilesIT extends SpectrumTest<Void> {
         downloadPage.getDownloadLinks().getFirst().click();
 
         assertThrows(TimeoutException.class, () -> checkDownloadedFile(FILE_TO_DOWNLOAD));
+
+        final Path downloadsFolder = Path.of(String.valueOf(configuration.getVars().get("downloadsFolder")));
+        assertTrue(Files.exists(downloadsFolder));
+        assertNotEquals(0, Objects.requireNonNull(downloadsFolder.toFile().listFiles()).length);
     }
 
     @Test
