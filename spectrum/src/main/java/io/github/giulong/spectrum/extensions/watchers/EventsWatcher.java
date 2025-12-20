cf1b76ab489a157a4d96b4e328c6a5090d7bbad6
Giulio Longfils <giuliolongfils@gmail.com>
2025-12-20 13:44:42 +0100

    refactor: annotating EventsWatcher extension context parameters as NotNull to comply with the parent signature

--- spectrum/src/main/java/io/github/giulong/spectrum/extensions/watchers/EventsWatcher.java
+++ spectrum/src/main/java/io/github/giulong/spectrum/extensions/watchers/EventsWatcher.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import io.github.giulong.spectrum.enums.Result;
 import io.github.giulong.spectrum.utils.events.EventsDispatcher;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.*;
 
@@ -17,17 +18,17 @@ public class EventsWatcher implements TestWatcher, BeforeTestExecutionCallback, 
     private final EventsDispatcher eventsDispatcher = EventsDispatcher.getInstance();
 
     @Override
-    public void beforeAll(final ExtensionContext context) {
+    public void beforeAll(@NotNull final ExtensionContext context) {
         notifyClass(context, BEFORE, null, Set.of(CLASS));
     }
 
     @Override
-    public void beforeEach(final ExtensionContext context) {
+    public void beforeEach(@NotNull final ExtensionContext context) {
         notifyTest(context, BEFORE, null, Set.of(TEST));
     }
 
     @Override
-    public void beforeTestExecution(final ExtensionContext context) {
+    public void beforeTestExecution(@NotNull final ExtensionContext context) {
         notifyTest(context, BEFORE_EXECUTION, null, Set.of(TEST));
 
         if (isTestFactory(context)) {
@@ -36,34 +37,34 @@ public class EventsWatcher implements TestWatcher, BeforeTestExecutionCallback, 
     }
 
     @Override
-    public void afterEach(final ExtensionContext context) {
+    public void afterEach(@NotNull final ExtensionContext context) {
         if (isTestFactory(context)) {
             notifyTest(context, AFTER, SUCCESSFUL, Set.of(TEST_FACTORY));
         }
     }
 
     @Override
-    public void afterAll(final ExtensionContext context) {
+    public void afterAll(@NotNull final ExtensionContext context) {
         notifyClass(context, AFTER, null, Set.of(CLASS));
     }
 
     @Override
-    public void testDisabled(final ExtensionContext context, final Optional<String> reason) {
+    public void testDisabled(@NotNull final ExtensionContext context, @NotNull final Optional<String> reason) {
         notifyTest(context, AFTER, DISABLED, Set.of(TEST));
     }
 
     @Override
-    public void testSuccessful(final ExtensionContext context) {
+    public void testSuccessful(@NotNull final ExtensionContext context) {
         notifyTest(context, AFTER, SUCCESSFUL, Set.of(TEST));
     }
 
     @Override
-    public void testAborted(final ExtensionContext context, final Throwable throwable) {
+    public void testAborted(@NotNull final ExtensionContext context, final Throwable throwable) {
         notifyTest(context, AFTER, ABORTED, Set.of(TEST));
     }
 
     @Override
-    public void testFailed(final ExtensionContext context, final Throwable exception) {
+    public void testFailed(@NotNull final ExtensionContext context, final Throwable exception) {
         notifyTest(context, AFTER, FAILED, Set.of(TEST));
     }
 
